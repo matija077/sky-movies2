88a44fc59a01215bbcc6e15c791665a59471d037
Matija077 <matija.prs@gmail.com>
2021-02-18 23:32:31 +0100

    add image component

---
+++ sky_movies/src/components/image/image.component.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+import {
+    ImageStyles,
+} from './image.styles';
+
+
+type ImagePropsType = {
+
+}
+
+function Image(props: ImagePropsType) {
+
+    return(
+        <div>aaaa</div>
+    );
+}
+
+export default Image;
---
+++ sky_movies/src/components/image/image.styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const ImageStyles = styled.imga`
+
+
+`;
+
+export {
+    ImageStyles,
+};
